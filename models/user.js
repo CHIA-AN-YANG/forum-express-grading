13a71f4640495ca74b9ae8ac5a0cf3f8c31aa78e
Chia-An Yang <chiaan.y.creativeworker@gmail.com>
2021-07-10 20:58:54 +0800

    Add addFollowing and removeFollowing to userController

--- models/user.js
+++ models/user.js
@@ -17,6 +17,16 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'UserId',
         as: 'LikedRestaurants'
       })
+      User.belongsToMany(User, {    //self-referential: 2組FK是放在同一個user model底下
+        through: models.Followship,
+        foreignKey: 'followingId',
+        as: 'Followers'
+      })
+      User.belongsToMany(User, {
+        through: models.Followship,
+        foreignKey: 'followerId',
+        as: 'Followings'
+      })
     }
   };
   User.init({
