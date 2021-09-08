30ebe9770d0642c7f099fbda59a4b87cf0156e7f
Chia-An Yang <chiaan.y.creativeworker@gmail.com>
2021-09-09 00:42:35 +0800

    refactor: refactor with prettier

--- app.js
+++ app.js
@@ -1,59 +1,64 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const app = express()
-if (process.env.NODE_ENV !== 'production') {require('dotenv').config() }
-const PORT = process.env.PORT || 3000
-const helpers = require('./_helpers.js')
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const PORT = process.env.PORT || 3000;
+const helpers = require("./_helpers.js");
 
-if (process.env.NODE_ENV === 'test'){
+if (process.env.NODE_ENV === "test") {
   app.use((req, res, next) => {
-    req.user = helpers.getUser(req)
-    next()})
+    req.user = helpers.getUser(req);
+    next();
+  });
 }
 
-
-app.engine('hbs', exphbs({ 
-  defaultLayout: 'main',
-  extname: '.hbs',
-  helpers:  require('./config/handlebars-helpers') 
-})) 
-app.set('view engine', 'hbs')
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+    helpers: require("./config/handlebars-helpers"),
+  })
+);
+app.set("view engine", "hbs");
 
 //folder paths
-const path = require('path');
-app.use('/static', express.static(path.join(__dirname, 'public')))
-app.use('/upload', express.static(__dirname + '/upload'))
+const path = require("path");
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.use("/upload", express.static(__dirname + "/upload"));
 
-const session = require('express-session')
-const passport = require('passport')
-const flash = require('connect-flash')
-app.use(session({
-  secret: process.env.SESSION_SECRET,    
-  resave: true, 
-  saveUninitialized: true 
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(flash())
-
+const session = require("express-session");
+const passport = require("passport");
+const flash = require("connect-flash");
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.use((req, res, next) => {
-  res.locals.success_messages = req.flash('success_messages')
-  res.locals.warning_messages = req.flash('warning_messages')
-  res.locals.currentuser =req.user
-  next()
-})
+  res.locals.success_messages = req.flash("success_messages");
+  res.locals.warning_messages = req.flash("warning_messages");
+  res.locals.currentuser = req.user;
+  next();
+});
 
 //set method-override
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-const methodOverride = require('method-override')
-app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 
-
-require('./routes')(app, passport)
+require("./routes")(app);
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
-})
+  console.log(`App listening at http://localhost:${PORT}`);
+});
 
-module.exports = app
+module.exports = app;
--- models/restaurant.js
+++ models/restaurant.js
@@ -1,36 +1,38 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Restaurant extends Model {
-
     static associate(models) {
-      Restaurant.belongsTo(models.Category, {foreignKey: { name: 'CategoryId' }})
-      Restaurant.hasMany(models.Comment)
+      Restaurant.belongsTo(models.Category, {
+        foreignKey: { name: "CategoryId" },
+      });
+      Restaurant.hasMany(models.Comment);
       Restaurant.belongsToMany(models.User, {
         through: models.Favorite,
-        foreignKey: 'RestaurantId',
-        as: 'FavoritedUsers'
-      })
+        foreignKey: "RestaurantId",
+        as: "FavoritedUsers",
+      });
       Restaurant.belongsToMany(models.User, {
         through: models.Like,
-        foreignKey: 'RestaurantId',
-        as: 'LikedUsers'
-      })
+        foreignKey: "RestaurantId",
+        as: "LikedUsers",
+      });
     }
-  };
-  Restaurant.init({
-    name: DataTypes.STRING(30),
-    tel: DataTypes.STRING(20),
-    address: DataTypes.STRING(127),
-    opening_hours: DataTypes.STRING,
-    description: DataTypes.TEXT(300),
-    image: DataTypes.STRING(300),
-    viewCounts: DataTypes.INTEGER.UNSIGNED
-  }, {
-    sequelize,
-    modelName: 'Restaurant',
-  });
+  }
+  Restaurant.init(
+    {
+      name: DataTypes.STRING(30),
+      tel: DataTypes.STRING(40),
+      address: DataTypes.STRING(127),
+      opening_hours: DataTypes.STRING,
+      description: DataTypes.TEXT(300),
+      image: DataTypes.STRING(300),
+      viewCounts: DataTypes.INTEGER.UNSIGNED,
+    },
+    {
+      sequelize,
+      modelName: "Restaurant",
+    }
+  );
   return Restaurant;
 };
