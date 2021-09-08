41aff1e658c7df49e4a3888ffb6a10f4c271dd2b
Chia-An Yang <chiaan.y.creativeworker@gmail.com>
2021-09-09 00:54:00 +0800

    feat: Add JWT token for api login system

--- config/passport.js
+++ config/passport.js
@@ -34,4 +34,29 @@ passport.deserializeUser((id, cb) => {
   })
 })
 
+//JWT strategy
+const jwt = require('jsonwebtoken')
+const passportJWT = require('passport-jwt')
+const ExtractJwt = passportJWT.ExtractJwt
+const JwtStrategy = passportJWT.Strategy
+
+let jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()     //token位置：authorization header 裡的 bearer 項目
+jwtOptions.secretOrKey = process.env.JWT_SECRET                          //使用密鑰來檢查 token 是否經過纂改
+
+let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+  User.findByPk(jwt_payload.id, {
+    include: [
+      { model: db.Restaurant, as: 'FavoritedRestaurants' },
+      { model: db.Restaurant, as: 'LikedRestaurants' },
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
+    ]
+  }).then(user => {
+    if (!user) return next(null, false)
+    return next(null, user)
+  })
+})
+passport.use(strategy)
+
 module.exports = passport
